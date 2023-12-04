fe177d0393644d6c5d335af67bceb15b2fdcd527
Manoel Ribeiro <ribeiromanoel2003@gmail.com>
2023-12-04 09:46:15 -0300

    add produtos.php

--- consultaSQL/produtos.php
+++ consultaSQL/produtos.php
@@ -5,24 +5,9 @@ $valor = $_POST['valor'];
 $quantidade = $_POST['quantidade'];
 $validade = $_POST['validade'];
 
-// echo "Produto: " . $produto . "<br>
-//      valor: " . $valor . "<br>
-//      quantidade: " . $quantidade . "<br>
-//      validade: " . $validade . "<br>";
-
-//CONEXÃO COM BANCO DE DADOS
-//$conn = new MySqli ('localhost', 'root', 'password', 'test');
-
 $conn = new MySqli('localhost', 'root', '', 'produtos_full');
 
-// if ($conn->connect_error) {
-//     die("Conection failed: " . $conn->connect_error);
-// } else {
-    
-//     echo "<br><div style='color:green'>Produto cadastrado com sucesso!</div>";
     $sql = "INSERT INTO `produtos`(`produto`, `valor`, `quantidade`, `validade`) VALUES ('$produto','$valor', '$quantidade', '$validade')";
-//     mysqli_query($conn, $sql);
-// }
 
 if (!$conn->connect_error) {
     if (mysqli_query($conn, $sql)) {
