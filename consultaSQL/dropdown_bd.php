dd644752ce08c0f8862e4e2edb08116cdaa22c00
Manoel Ribeiro <ribeiromanoel2003@gmail.com>
2023-12-12 10:04:19 -0300

    aula dia 12/12

---
+++ consultaSQL/dropdown_bd.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once('conexao.php');
+include_once('head.php');
+
+
+$sql = "SELECT id, produto FROM produtos ORDER BY produto";
+$result = $conn->query($sql);
+
+if ($result) {
+    echo("
+    <div class='container card mt-2'>
+        <h2>Lista de Produtos</h2>
+        <form action='./dropdown_pesquisa.php' method='POST'>
+            <select class='form-select' name='id_produto'>"    
+    );
+    while ($linha = mysqli_fetch_array($result)) {
+        echo("
+                <option value='$linha[id]'>$linha[produto]</option>
+        ");
+    }
+    echo("
+            </select>
+            <input class='btn btn-danger mt-2 mb-2' type='submit' value='Pesquisar'></input>
+        </form>
+    </div>
+        ");
+}
