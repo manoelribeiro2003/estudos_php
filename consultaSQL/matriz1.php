a03351e03c867eb75d17715d11e2966b49d1f803
Lohan1 <italolohan244@gmail.com>
2024-01-15 08:05:36 -0300

    cpp

---
+++ consultaSQL/matriz1.php
@@ -0,0 +1,11 @@
+<?php
+
+$produtos1 = array(
+    array(4.50, 50),
+    array(6.50, 30),
+    array(4.00, 35),
+);
+
+echo ("Valor: ".$produtos1[0][0]." , Quantiadade: ".$produtos1[0][1]. "<br>");
+echo ($produtos1[1][0]);
+?>
