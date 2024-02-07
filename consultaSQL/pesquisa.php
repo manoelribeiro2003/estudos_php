8808cdcc449509d79c9ad6eb725de0cdc23902ef
Lohan1 <italolohan244@gmail.com>
2024-02-07 11:15:20 -0300

    aula dia 07/02 criando classes conexao banco de dados

--- consultaSQL/pesquisa.php
+++ consultaSQL/pesquisa.php
@@ -23,40 +23,41 @@ $linha = mysqli_fetch_array($resultado);
 if ($linha) {
     echo "
     <body>
-    <div class='card container mt-3 '>
-        <div class='mt-2'>
-            <h2 style='text-align: center;' class='mt-0'>VER PRODUTO</h2>
+        <div class='card container mt-3 '>
+            <div class='mt-2'>
+                <h2 style='text-align: center;' class='mt-0'>VER PRODUTO</h2>
+            </div>
+            <form action='atualizar.php' method='POST'>
+                <div class='mb-3'>
+                    <label class='form-label'>Produto*</label>
+                    <input value='$linha[produto]' type='text' class='form-control' id='produto' name='produto' onblur='V_produto(this)' required>
+                    <div id='alertaProduto' class='form-text'></div>
+                </div>
+                <div class='mb-3'>
+                    <label class='form-label'>Valor*</label>
+                    <input value='$linha[valor]' type='text' class='form-control' id='valor' name='valor' onblur='V_valor(this)' required>
+                    <div id='alertaValor' class='form-text'></div>
+                </div>
+                <div class='mb-3'>
+                    <label class='form-label'>Quantidade*</label>
+                    <input value='$linha[quantidade]' type='text' class='form-control' id='quantidade' name='quantidade' onblur='V_quantidade(this)'
+                        required>
+                    <div id='alertaQuantidade' class='form-text'></div>
+                </div>
+                <div class='mb-3'>
+                    <label class='form-label'>Validade*</label>
+                    <input value='$linha[validade]' type='date' class='form-control' id='validade' name='validade' onblur='V_validade(this)'
+                        required>
+                </div>
+                    <div id='alertaValidade' class='form-text'></div>
+                <input type='hidden' name= 'id' value='$linha[id]'>
+                <div class='mb-3'>
+                    <input type='submit' class='btn btn-primary' onblur='V_cadastrar(this)' value='Atualizar'>
+                </div>
+            </form>
         </div>
-        <form action='atualizar.php' method='POST'>
-            <div class='mb-3'>
-                <label class='form-label'>Produto*</label>
-                <input value='$linha[produto]' type='text' class='form-control' id='produto' name='produto' onblur='V_produto(this)' required>
-                <div id='alertaProduto' class='form-text'></div>
-            </div>
-            <div class='mb-3'>
-                <label class='form-label'>Valor*</label>
-                <input value='$linha[valor]' type='text' class='form-control' id='valor' name='valor' onblur='V_valor(this)' required>
-                <div id='alertaValor' class='form-text'></div>
-            </div>
-            <div class='mb-3'>
-                <label class='form-label'>Quantidade*</label>
-                <input value='$linha[quantidade]' type='text' class='form-control' id='quantidade' name='quantidade' onblur='V_quantidade(this)'
-                    required>
-                <div id='alertaQuantidade' class='form-text'></div>
-            </div>
-            <div class='mb-3'>
-                <label class='form-label'>Validade*</label>
-                <input value='$linha[validade]' type='date' class='form-control' id='validade' name='validade' onblur='V_validade(this)'
-                    required>
-            </div>
-                <div id='alertaValidade' class='form-text'></div>
-            <input type='hidden' name= 'id' value='$linha[id]'>
-            <div class='mb-3'>
-                <input type='submit' class='btn btn-primary' onblur='V_cadastrar(this)' value='Atualizar'>
-            </div>
-        </form>
-    </div>
-    </body>";
+    </body>
+    ";
 } else {
     echo(red("<br><div>Produto não localizado!</div>"));
 }
